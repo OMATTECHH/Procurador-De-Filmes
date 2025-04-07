1141c7830168946945e54fdd472640315c4b1b0a
Mateus <devteus.officeti@gmail.com>
2025-04-07 09:38:50 -0300

    Primeiro commit - protegendo API_KEY

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
-API_KEY = "7df19500"
-BASE_URL = "http://www.omdbapi.com/"
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+# Agora pega a chave da variável de ambiente
+API_KEY = os.getenv("API_KEY")
+
+# Usa a chave normalmente
+url = "https://api.themoviedb.org/3/movie/"
+
+print(url)
